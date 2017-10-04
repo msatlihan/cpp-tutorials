383e99dc1a06532cf8203da62b32abaaf0d469a6
msatlihan <msatlihan@gmail.com>
2017-10-04 12:37:09 +0300

    add file

---
+++ 02-using-namespace.cpp
@@ -0,0 +1,15 @@
+// Use namespace in order to get rid of writing `std` multiple times.
+#include <iostream>
+
+using namespace std; // Tells the compiler we assume using the standard library.
+
+int main()
+{
+    cout << "Writing std:: is pain, ";
+    cout << "Use namespace for it!\n";
+    return 0; // This is optional here.
+}
+
+// Be careful! If the commands are not explicitly defined, it is possible when you
+// add your code to a larger project, the code might referens a command from a
+// different library.
