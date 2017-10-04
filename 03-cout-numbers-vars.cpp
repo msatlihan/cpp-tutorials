0518c5be1b27b670a92f2aa9a20b5f761c719b1a
msatlihan <msatlihan@gmail.com>
2017-10-04 12:45:19 +0300

    add file

---
+++ 03-cout-numbers-vars.cpp
@@ -0,0 +1,15 @@
+// Tutorials are coded by Musa Atlıhan with the help of Udacity C++ lessons.
+// Credits https://www.udacity.com
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int my_variable = 42;
+
+    cout << "Printing strings, numbers and variables.\n";
+    cout << 42;
+    cout << my_variable;
+}
